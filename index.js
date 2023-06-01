6c9e65a45792bcf4553b7dc7cdf220db19bc759a
Erick El nino <erick.tshimbombo@yahoo.com>
2023-06-01 22:05:47 +0200

    update

--- index.js
+++ index.js
@@ -9,19 +9,19 @@ let num1 = parseInt(process.argv[2]);
 let num2 = parseInt(process.argv[4]);
 let choice  = process.argv[3].trim();
 
-if(choice.includes("DIV"))
+if(choice === "DIV")
 {
     console.log(`Number 1: ${num1} Number2: ${num2} Operation: %`);
     console.log(div(num1,num2));
-}else if(choice.includes("SUB"))
+}else if(choice === "SUB")
 {
     console.log(`Number 1: ${num1} Number2: ${num2} Operation: -`);
    console.log(sub(num1,num2))
-}else if(choice.includes("ADD"))
+}else if(choice === "ADD")
 {
     console.log(`Number 1: ${num1} Number2: ${num2} Operation: +`);
    console.log(add(num1,num2));
-}else if(choice.includes("MULTIPLE"))
+}else if(choice === "MULTIPLE")
 {
     console.log(`Number 1: ${num1} Number2: ${num2} Operation: X`);
     console.log(multiply(num1,num2));
