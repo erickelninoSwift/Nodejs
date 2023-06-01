e7ee5075255a57a50a36008380f15ad6d31e4c28
Erick El nino <erick.tshimbombo@yahoo.com>
2023-06-01 17:54:53 +0200

    update operation

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const mydiv = require("./Calculator/div");
 const multiplication = require("./Calculator/multiply");
 const Addition = require("./Calculator/add");
 const substraction = require("./Calculator/sub");
-const { parse } = require("path");
 
 
 
@@ -10,16 +9,20 @@ const num1 = parseInt(process.argv[2]);
 const num2 = parseInt(process.argv[4]);
 const choice  = process.argv[3];
 
-if(choice === "div")
+if(choice === "DIVIDE")
 {
+    console.log(`Number 1: ${num1} Number2: ${num2} Operation: %`);
     console.log(mydiv(num1,num2));
-}else if(choice === "sub")
+}else if(choice === "SUB")
 {
+    console.log(`Number 1: ${num1} Number2: ${num2} Operation: -`);
    console.log(substraction(num1,num2))
-}else if(choice === "add")
+}else if(choice === "ADD")
 {
+    console.log(`Number 1: ${num1} Number2: ${num2} Operation: +`);
    console.log(Addition(num1,num2));
-}else if(choice === "mul")
+}else if(choice === "MULTIPLE")
 {
+    console.log(`Number 1: ${num1} Number2: ${num2} Operation: X`);
     console.log(multiplication(num1,num2));
 }
