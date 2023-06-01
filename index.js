401acb4198a89494403798c8f9dd5f738f203f85
Erick El nino <erick.tshimbombo@yahoo.com>
2023-06-01 17:47:30 +0200

    update functions

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-const mydiv = require("../Calculator/div");
-const multiplication = require("../Calculator/multiply");
-const Addition = require("../Calculator/add");
-const substraction = require("../Calculator/sub");
+const mydiv = require("./Calculator/div");
+const multiplication = require("./Calculator/multiply");
+const Addition = require("./Calculator/add");
+const substraction = require("./Calculator/sub");
+const { parse } = require("path");
 
 
-console.log(mydiv(10,2));
+
+const num1 = parseInt(process.argv[2]);
+const num2 = parseInt(process.argv[4]);
+const choice  = process.argv[3];
+
+if(choice === "div")
+{
+    console.log(mydiv(num1,num2));
+}else if(choice === "sub")
+{
+   console.log(substraction(num1,num2))
+}else if(choice === "add")
+{
+   console.log(Addition(num1,num2));
+}else if(choice === "mul")
+{
+    console.log(multiplication(num1,num2));
+}
